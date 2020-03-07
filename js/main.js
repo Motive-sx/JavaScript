d2af88c328c6f228cfdd4231cfa33d0a5038be7d
Motive-sx <sx.motive@gmail.com>
2020-03-07 16:55:20 +0500

    Урок 2 (Уровень 2)

---
+++ js/main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+class shoppingBag {
+    constructor(bag = '.bag-wrapper') {
+        this.bag = bag;
+        this.selectedItems = selectedItems;
+    }
+    
+    sumOfSelected() {
+        ...
+    }
+}
+
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            {
+                id: 1,
+                title: 'Notebook',
+                price: 2000
+            },
+            {
+                id: 2,
+                title: 'Mouse',
+                price: 20
+            },
+            {
+                id: 3,
+                title: 'Keyboard',
+                price: 200
+            },
+            {
+                id: 4,
+                title: 'Gamepad',
+                price: 50
+            },
+        ]
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+    }
+
+     sumPrices() {
+           ...
+     }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Buy</button>
+                </div>`;
+    }
+}
+
+let list = new ProductsList();
+list.render();
+
+
+console.log(list.sumPrices());
